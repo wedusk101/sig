32f0cf14eb4357fa826a565484abae0ac39c26ee
Marcelo Kallmann <marcelo.kallmann@gmail.com>
2019-05-04 19:21:33 -0700

    bat upd

--- vs2017/buildlibs.bat
+++ vs2017/buildlibs.bat
@@ -2,13 +2,16 @@
 call "C:\Program Files (x86)\Microsoft Visual Studio\2017\Community\Common7\Tools\VsDevCmd.bat"
 
 REM q:quiet m:minimum n:normal
-set "PARAMS=/m /nr:false /nologo /verbosity:q /t:Build /p:Platform=x86"
+set "PARAMS=/m /nr:false /nologo /verbosity:q /t:Build"
 
 rem ========== build cmds ==========
 call "cleanall.bat"
-call :build siglibs.sln Release
-call :build siglibs.sln ReleaseDLL
-call :build siglibs.sln Debug
+call :build siglibs.sln Release x86
+call :build siglibs.sln ReleaseDLL x86
+call :build siglibs.sln Debug x86
+call :build siglibs.sln Release x64
+call :build siglibs.sln ReleaseDLL x64
+call :build siglibs.sln Debug x64
 call "clean.bat"
 pause
 goto end
@@ -16,8 +19,8 @@ goto end
 rem ========== functions ==========
 :build
 SETLOCAL
-@echo Building: %1 %2...
-MSBuild %1 %PARAMS% /p:Configuration=%2
+@echo Building: %1 %2 %3...
+MSBuild %1 %PARAMS% /p:Configuration=%2 /p:Platform=%3
 ENDLOCAL
 goto end
 
