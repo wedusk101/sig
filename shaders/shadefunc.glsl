31984a01d53f9e69bacf2a7a98cb4907e80aa21f
Marcelo Kallmann <marcelo.kallmann@gmail.com>
2020-04-18 10:39:35 -0700

    shaders upd to color back faces

--- shaders/shadefunc.glsl
+++ shaders/shadefunc.glsl
@@ -9,9 +9,13 @@ vec4 shade ( vec3 p, vec3 n, vec3 lp, vec3 la, vec3 ld, vec3 ls, vec3 ka, vec3 k
 	vec3 r = reflect ( -l, n );         // the reflected light ray
 	//vec3 r = normalize(l+v);  // testing the half-vector instead of reflected ray
 
+	// LightDev: set Front or BackAndFront rendering as selectable via uniforms
 	float dotln = dot(l,n);
+	if (dotln<0) dotln=-dotln; // to get black in back faces use: max(dotln,0.0)
+	float dotvr = dot(v,r);
+	if (dotvr<0) dotvr=-dotvr; // or: max(dot(v,r),0.0)
 	vec3 amb = la * ka;
-	vec3 dif = ld * kd * max(dotln,0.0); // lambertian component
-	vec3 spe = max(dotln,0.0) * ls * ks * pow ( max(dot(v,r),0.0), sh );
+	vec3 dif = ld * kd * dotln; // lambertian component
+	vec3 spe = dotln * ls * ks * pow ( dotvr, sh );
 	return vec4 ( amb + dif + spe + emi, alpha );
 }
