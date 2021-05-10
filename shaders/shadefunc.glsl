ca7ccb75941a1d1c98d8793df891187f5d4b5579
Marcelo Kallmann <marcelo.kallmann@gmail.com>
2021-05-09 22:21:19 -0700

    fix for glsl 4.6 compilation

--- shaders/shadefunc.glsl
+++ shaders/shadefunc.glsl
@@ -11,9 +11,9 @@ vec4 shade ( vec3 p, vec3 n, vec3 lp, vec3 la, vec3 ld, vec3 ls, vec3 ka, vec3 k
 
 	// LightDev: set Front or BackAndFront rendering as selectable via uniforms
 	float dotln = dot(l,n);
-	if (dotln<0) dotln=-dotln; // to get black in back faces use: max(dotln,0.0)
+	if (dotln<0.0) dotln=-dotln; // to get black in back faces use: max(dotln,0.0)
 	float dotvr = dot(v,r);
-	if (dotvr<0) dotvr=-dotvr; // or: max(dot(v,r),0.0)
+	if (dotvr<0.0) dotvr=-dotvr; // or: max(dot(v,r),0.0)
 	vec3 amb = la * ka;
 	vec3 dif = ld * kd * dotln; // lambertian component
 	vec3 spe = dotln * ls * ks * pow ( dotvr, sh );
